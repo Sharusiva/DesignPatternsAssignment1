d03d614323b574fe2e9bdb895b20470b1cd07f92
Sharusiva <sharujan.sivanandam@ontariotechu.net>
2025-09-20 23:47:12 -0400

    Cleaned up the code to make sense, also changes made to the UML diagram to make the Factory Method use an Abstractclass to generate smart lock and smart bulb instead of interfaces

--- Assignment1/src/App.java
+++ Assignment1/src/App.java
@@ -1,31 +1,75 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Demonstrate BrandABulb
+        // Create BrandA devices using factory
+        SmartDeviceFactory brandAFactory = new BrandAFactory();
+        SmartBulb bulbA = brandAFactory.createSmartBulb();
+        SmartLock lockA = brandAFactory.createSmartLock();
+
+        System.out.println("=== Testing BrandA Devices ===");
+        bulbA.turnOn();
+        bulbA.setBrightness();
+        System.out.println("BrandA Bulb power usage: " + bulbA.setPowerUsage("dummyFile") + "W");
+        bulbA.turnOff();
+
+        lockA.unlock();
+        System.out.println("BrandA Lock status (unlocked): " + lockA.lockStatus);
+        lockA.lock();
+        System.out.println("BrandA Lock status (locked): " + lockA.lockStatus);
+        lockA.autoLock();
+        System.out.println("BrandA Lock status (auto-locked): " + lockA.lockStatus);
+
+        System.out.println("\n=== Testing BrandB Devices ===");
+        // Create BrandB devices using factory
+        SmartDeviceFactory brandBFactory = new BrandBFactory();
+        SmartBulb bulbB = brandBFactory.createSmartBulb();
+        SmartLock lockB = brandBFactory.createSmartLock();
+
+        bulbB.turnOn();
+        bulbB.setBrightness();
+        System.out.println("BrandB Bulb power usage: " + bulbB.setPowerUsage("dummyFile") + "W");
+        bulbB.turnOff();
+
+        lockB.unlock();
+        System.out.println("BrandB Lock status (unlocked): " + lockB.lockStatus);
+        lockB.lock();
+        System.out.println("BrandB Lock status (locked): " + lockB.lockStatus);
+        lockB.autoLock();
+        System.out.println("BrandB Lock status (auto-locked): " + lockB.lockStatus);
+
     }
 }
 
-interface SmartBulb {
+abstract class SmartBulb {
     float powerUsage;
     String colour;
     int wattage;
 
-    void setBrightness();
+    abstract void setBrightness();
 
-    void turnon();
+    abstract void turnOn();
 
-    void turnOff();
+    abstract void turnOff();
 
-    float setPowerUsage(String file);
+    abstract float setPowerUsage(String file);
 
 }
 
-interface SmartLock {
+abstract class SmartLock {
+    float batteryConsumption;
+    String connectivity;
+    boolean lockStatus; // lock is initially unlocked
 
-    void lock();
+    void lock() {
+        lockStatus = true;
+    }
 
-    void Unlock();
+    void unlock() {
+        lockStatus = false;
+    }
 
-    void autoLock();
+    abstract void autoLock();
 }
 
 interface SmartDeviceFactory {
@@ -35,28 +79,89 @@ interface SmartDeviceFactory {
 }
 
 // BrandA implementations
-class BrandABulb implements SmartBulb {
+class BrandABulb extends SmartBulb {
+
+    @Override
     public void turnOn() {
         System.out.println("BrandA Bulb turned on.");
     }
+
+    @Override
+    public void turnOff() {
+        System.out.println("BrandA Bulb turned off.");
+    }
+
+    @Override
+    public void setBrightness() {
+        System.out.println("BrandA Bulb brightness set.");
+    }
+
+    @Override
+    public float setPowerUsage(String file) {
+        return 56.0f;
+    }
+
 }
 
-class BrandALock implements SmartLock {
-    public void turnOn() {
+class BrandALock extends SmartLock {
+    @Override
+    public void unlock() {
+        super.unlock();
         System.out.println("BrandA Lock unlocked.");
+    }
+
+    @Override
+    public void lock() {
+        super.lock();
+        System.out.println("BrandA Lock locked.");
+    }
+
+    @Override
+    public void autoLock() {
+        System.out.println("BrandA Lock auto-locked.");
     }
 }
 
 // BrandB implementations
-class BrandBBulb implements SmartBulb {
+class BrandBBulb extends SmartBulb {
+
+    @Override
     public void turnOn() {
         System.out.println("BrandB Bulb turned on.");
     }
+
+    @Override
+    public void turnOff() {
+        System.out.println("BrandB Bulb turned off.");
+    }
+
+    @Override
+    public void setBrightness() {
+        System.out.println("BrandB Bulb brightness set.");
+    }
+
+    @Override
+    public float setPowerUsage(String file) {
+        return 12.0f;
+    }
 }
 
-class BrandBLock implements SmartLock {
-    public void turnOn() {
+class BrandBLock extends SmartLock {
+    @Override
+    public void unlock() {
+        super.unlock();
         System.out.println("BrandB Lock unlocked.");
+    }
+
+    @Override
+    public void lock() {
+        super.lock();
+        System.out.println("BrandB Lock locked.");
+    }
+
+    @Override
+    public void autoLock() {
+        System.out.println("BrandB Lock auto-locked.");
     }
 }
 
